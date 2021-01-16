bcd451e5bebba095835def7c95bb56a2181fed6b
Wasilios Goutas <wasili@goutas.de>
2021-01-16 16:49:23 +0100

    added argument parser for ClearML configuration demonstration

--- ClearML_regression.py
+++ ClearML_regression.py
@@ -7,14 +7,44 @@ Created on Fri Jan 15 19:39:37 2021
 """
 import argparse
 import yfinance as yf
+import pandas as pd
+import plotly.express as px
 from clearml import Task
 
-#task = Task.init(project_name='Regression test with configurable arguments', task_name='finance')
+task = Task.init(project_name='first ClearML steps', task_name='finance')
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--symbol', help='symbol used for regression', default='AAPL')
-args = parser.parse_args()
+#%%
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--symbol', help='symbol used for regression', default='AAPL')
+    args = parser.parse_args()
 
-tickerData = yf.Ticker(args.symbol)
-tickerDf = tickerData.history(period='max')
-type(tickerDf)
+    tickerData      = yf.Ticker(args.symbol)
+    tickerDf        = tickerData.history(period='max', interval='1d')[['Open', 'High', 'Low', 'Close', 'Volume']]
+    tickerDf_lag1d  = tickerDf - tickerDf.shift(1)
+    tickerDf_Change = tickerDf_lag1d / tickerDf * 100
+    tickerDf_Change.columns += '_pcent'
+    
+    df = pd.concat([tickerDf.Close, tickerDf_Change.Close_pcent], axis=1)
+    #setattr(df, 'ticker', args.symbol)          
+    #fig = plot_(df, show=False)         
+    fig = px.line(df, title=args.symbol)
+    task.get_logger().report_plotly(title='finance', series=args.symbol, iteration=0, figure=fig)
+    fig.show()
+    return
+
+
+def plot_(df, show=False):
+    import plotly.io as pio
+    #pio.renderers.default='browser'
+    pio.renderers.default='svg'
+    fig = px.line(df, title=getattr(df, 'ticker'))
+    if show: fig.show()
+    return fig
+
+#%%
+if __name__ == '__main__':
+    main()
+    print('done')
+
+#%%
